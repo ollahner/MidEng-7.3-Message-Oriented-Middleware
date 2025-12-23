a5c42889b507b94dc5f2998a6fe31544cdc17499
olive <olahner@student.tgm.ac.at>
2025-12-23 10:39:19 +0100

    Commit

--- src/main/java/com/example/demo/MessageConsumer.java
+++ src/main/java/com/example/demo/MessageConsumer.java
@@ -1,14 +1,38 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@Service
 public class MessageConsumer {
 
-    @KafkaListener(topics = "quickstart-events")
+    private static final Logger logger = LoggerFactory.getLogger(MessageConsumer.class);
+
+    private final List<String> storageReport = new ArrayList<>();
+
+    @KafkaListener(topics = "warehouse-data", groupId = "central-office-group")
     public void processMessage(String content) {
-        System.out.println( "Read from Message Queue: " + content);
+        logger.info("Received Message: " + content);
+
+        synchronized (storageReport) {
+            storageReport.add(content);
+        }
+
+        System.out.println("Log: " + content);
+    }
+
+    @GetMapping("/management/report")
+    public List<String> getManagementReport() {
+        return storageReport;
     }
 
 }
--- src/main/java/com/example/demo/MessageProducer.java
+++ src/main/java/com/example/demo/MessageProducer.java
@@ -8,16 +8,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Component
 public class MessageProducer {
 
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    @GetMapping("/send")
-    public String sendMessage(@RequestParam(value = "message", defaultValue = "") String message) {
-        kafkaTemplate.send("quickstart-events", message);
-        return "Message '" + message + "' sent.";
+    private static final String TOPIC ="warehouse-data";
+
+    @GetMapping("/warehouse/send")
+    public String sendMessage(@RequestParam(value = "data", defaultValue = "") String data) {
+        if (data.isEmpty()) return "No data provided";
+        kafkaTemplate.send(TOPIC, data);
+        return "SUCCESS";
     }
 
 }
